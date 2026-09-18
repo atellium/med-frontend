3ba8ade34019607e87e07fb88eb8664f6e1ef832
Rupam Dhara <thedevgenius83@gmail.com>
2026-09-18 17:05:42 +0530

    SEO added

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,12 +2,22 @@ import type { MetadataRoute } from "next";
 import { getSiteUrl } from "@/lib/site-url";
 
 export default function robots(): MetadataRoute.Robots {
+  const siteUrl = getSiteUrl();
+
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/profile"],
+      disallow: [
+        "/api/",
+        "/profile",
+        "/saved",
+        "/test",
+        "/*/manage/",
+        "/provider/*/manage/",
+      ],
     },
-    sitemap: new URL("/sitemap.xml", getSiteUrl()).toString(),
+    sitemap: new URL("/sitemap.xml", siteUrl).toString(),
+    host: siteUrl.origin,
   };
 }
